e2ec6a5059cbd11671c697f5bbacb047120d5fb5
troido <troido@hotmail.com>
2017-11-12 20:01:05 +0100

    massively improved help. untested though

--- modules/help.py
+++ modules/help.py
@@ -4,39 +4,81 @@ from subbot import SubBot
 class HelpBot(SubBot):
     
     name = "helpbot"
-    commands = {"!rollcall", "!gamebothelp", "!games"}
+    commands = {"!rollcall", "!gamebothelp", "!gbhelp", "!gbmodules", "!games"}
     description = "Show all functionality of this bot."
     botname = "gamebot"
     spamfree = {"#tildetown", "#gamebotshort"}
     
-    games = {"grunkbot", "hhgttgbot", "sofarbot"}
+    games = {"grunk", "hhgttg", "sofar"}
     
     def on_command(self, command, args, chan, sender, text):
-        botmodules = self.bot.subbots.values()
-        if command == "!gamebothelp":
-            if chan in self.spamfree:
-                self.reply(chan, "To prevent spamming the channel, full help is not available here. Try in another channel")
-            else:
-                helptext = "{} is ~troido's irc bot. It has several kinds of functionality:\n{}".format(
-                    self.botname,
-                    '\n'.join(subbot.get_description() for subbot in botmodules))
-                for line in helptext.split("\n"):
-                    self.reply(chan, line)
-        elif command == "!rollcall":
-            self.reply(chan, "{botname} here. I am a modular bot. Currently I have these modules loaded: {modules}. I respond to these commands: {allcommands}. For more information, say !gamebothelp (long list).".format(
+        
+        self.reply(chan, self.getHelp(command, args.split(), channel in self.spamfree))
+    
+    def getHelp(self, command, args, spamFree=False):
+        
+        if command == "!rollcall":
+            return self.rollcall()
+        
+        if command == "!gamebothelp" or command == "!gbhelp":
+            if len(args) < 1:
+                return self.shortHelp()
+            if args[0] in self.bot.subbots:
+                return self.moduleHelp(args[0])
+            if args[0] == "all" or args[0] == "*":
+                if spamFree:
+                    return "to avoid clogging the main channel, the full list is not available here. Try another channel"
+                return self.longHelp()
+            if args[0] == "modules":
+                return self.getModules()
+        
+        if command == "!gamebotmodules" or command == "!gbmodules":
+            return self.getModules
+        
+        if command == "!games":
+            if spamFree:
+                return "to avoid clogging the main channel, this list is not available here. Try another channel"
+            return self.gameHelp()
+    
+    
+    def rollcall(self):
+        return self.format("{botname} here. I am a modular bot. Currently I have these modules loaded: {modules}. I respond to these commands: {allcommands}. For all more information, say '!gamebothelp' or '!gbhelp'")
+    
+    def shortHelp(self):
+        return self.format("{botname} is ~troido's irc bot. It has several independently running modules. To see a list of currently loaded modules say '!gbmodules'. To see information on a specific module, say '!gbhelp <module>' where <module> is the name of the module. For a list of information on all modules, say '!gbhelp *' (unavailable in channel #tildetown)")
+    
+    def longHelp(self):
+        helptext = self.format("{botname} is ~troido's irc bot. It has several kinds of functionality:\n{allhelp}")
+        for line in helptext.split("\n"):
+            self.reply(chan, line)
+        return helptext
+    
+    def moduleHelp(self, name):
+        modules = self.bot.subbots
+        if name not in modules:
+            return "module {} not found".format(name)
+        return modules[name].get_description()
+    
+    def allModuleHelp(self):
+        return '\n'.join(self.moduleHelp(module) for module in self.bot.subbots)
+    
+    def getModules(self):
+        return ', '.join(name for name in self.bot.subbots)
+    
+    def gameHelp(self, spamfree=False):
+        help = "Hi I'm gamebot. I currently have these games available:"
+        for name, game in self.bot.subbots.items():
+            if name in self.games:
+                help += "{name}. Startcommand: {startcommand}, runcommand: {command}. Channels: {channels}".format(name=game.name, startcommand=game.startcommand, command=game.runcommand, channels = ', '.join(game.channels))
+        help += "Start a session of a game by running its startcommand. Once a session is running, you can control the game with the runcommand. Everyting you enter after the runcommand will be passed as input to the game."
+        return help
+    
+    def format(self, text):
+        return text.format(
                 botname = self.botname,
-                modules = ', '.join(subbot.name for subbot in botmodules),
-                allcommands = ', '.join(set().union(*(subbot.commands for subbot in botmodules)))))
-        elif command == "!games":
-            if chan in self.spamfree:
-                self.reply(chan, "To prevent spamming the channel, the game list is not available here. Try in another channel")
-            else:
-                self.reply(chan, "Hi I'm gamebot. I currently have these games available:")
-                for name, game in self.bot.subbots.items():
-                    if name in self.games:
-                        self.reply(chan, "{name}. Startcommand: {startcommand}, runcommand: {command}. Channels: {channels}".format(name=game.name, startcommand=game.startcommand, command=game.runcommand, channels = ', '.join(game.channels)))
-                self.reply(chan, "Start a session of a game by running its startcommand. Once a session is running, you can control the game with the runcommand. Everyting you enter after the runcommand will be passed as input to the game.")
-                
+                modules = self.getModules,
+                allcommands = ', '.join(set().union(*(subbot.commands for subbot in self.bot.subbots.values()))),
+                allhelp = self.allModuleHelp())
 
 
 
