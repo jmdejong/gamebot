3af08e7b8240d0be3c6f889fcb2fd8025d38fc9d
troido <troido@hotmail.com>
2017-11-14 22:44:53 +0100

    made veryswedish and veryveryswedish commands

--- modules/swedish.py
+++ modules/swedish.py
@@ -4,15 +4,17 @@ from subbot import SubBot
 class SwedishTranslationBot(SubBot):
     
     name = "swedish"
-    commands = {"!swedish"}
+    commands = {"!swedish", "!veryswedish", "!veryveryswedish"}
     description = "makes texts swedish with help from yaib. See also: https://www.youtube.com/watch?v=RqvCNb7fKsg"
     
     
     def on_command(self, command, args, chan, sender, text):
         self.reply(chan, args)
         self.reply(chan, "s//f")
+        if "very" in command:
+            self.reply(chan, "s//f")
+        if "veryvery" in command:
+            self.reply(chan, "s//f")
         
-
-
 
 BotModule = SwedishTranslationBot
