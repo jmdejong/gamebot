190dad4504a14d455fb6812df3654d836a7710c3
troido <troido@hotmail.com>
2018-02-16 15:09:25 +0100

    disabled what for tildetown channel

--- modules/what.py
+++ modules/what.py
@@ -8,6 +8,9 @@ class WhatBot(SubBot):
     description = "Get the description of some word by calling other bots and using duckduckgo"
     
     def on_command(self, command, args, chan, *_args, **_kwargs):
+        if chan == "#tildetown":
+            self.reply(chan, "To prevent flooding the chat, !what is not available in this #tildetown. Try runing it in #bots or another channel")
+            return
         self.reply(chan, "!define " + args)
         self.reply(chan, ",ud " + args)
         ddgbot = self.bot.commands["!ddg"]
