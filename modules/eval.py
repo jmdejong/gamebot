3284e44177bfdc7454174e5949bb2597faa2866a
troido <troido@protonmail.com>
2018-09-19 21:53:06 +0200

    disabled 'open' in asteval

--- modules/eval.py
+++ modules/eval.py
@@ -10,7 +10,9 @@ class Evaluator(SubBot):
     description = "evaluates a (python-like) expression using asteval"
     
     def __init__(self):
-        self.aeval = asteval.Interpreter(no_print=True, no_raise=True, no_assert=True, blacklist_builtins=True, no_functiondef=True)
+        symtable = asteval.make_symbol_table()
+        del symtable["open"]
+        self.aeval = asteval.Interpreter(symtable, no_print=True, no_raise=True, no_assert=True, blacklist_builtins=True, no_functiondef=True)
     
     def on_command(self, command, args, chan, *_args, **_kwargs):
         try:
