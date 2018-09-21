e77c4d3293b67ac889c557ff71cc610cc1afe029
troido <troido@protonmail.com>
2018-09-22 00:24:49 +0200

    improved evaluator with hy and with better errors and asteval

--- modules/eval.py
+++ modules/eval.py
@@ -1,25 +1,54 @@
 
+import ast
 import asteval
+# using a modified version of asteval (unless it will be merged)
+# see https://github.com/jmdejong/asteval/tree/gb
+
+try:
+    import hy
+    from hy.compiler import hy_compile
+except ImportError:
+    hy = None
 
 from subbot import SubBot
 
 class Evaluator(SubBot):
     
     name = "eval"
-    commands = {"!eval"}
+    commands = {"!eval", "!python", "!python3", "!hy"}
     description = "evaluates a (python-like) expression using asteval"
     
     def __init__(self):
         symtable = asteval.make_symbol_table()
         del symtable["open"]
-        self.aeval = asteval.Interpreter(symtable, no_print=True, no_raise=True, no_assert=True, blacklist_builtins=True, no_functiondef=True)
+        self.aeval = asteval.Interpreter(symtable, no_print=True, blacklist_builtins=True)
     
     def on_command(self, command, args, chan, *_args, **_kwargs):
+        if command == "!hy":
+            if hy is None:
+                self.reply(chan, "Hy is not installed")
+                return
+            try:
+                tree = hy_compile(hy.read_str(args), "__main__")
+            except Exception as ex:
+                # I don't like catching all but I don't know how many errors this can throw
+                self.reply(chan, "Parsing Error: "+str(ex))
+                return
+        else:
+            try:
+                tree = ast.parse(args)
+            except SyntaxError as ex:
+                self.reply(chan, "Syntax Error: "+str(ex))
+        returnval = None
+        error = None
         try:
-            reply = str(self.aeval(args))
-        except Exception as e:
-            reply = str(e)
-        self.reply(chan, reply)
+            returnval = str(self.aeval(args))
+        except asteval.EvalError as e:
+            error = e
+        if returnval:
+            self.reply(chan, returnval)
+        if error:
+            self.reply(chan, error)
 
 
 
