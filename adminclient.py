52ebc6ebcbac122602897f0106db05399757e3cc
troido <troido@protonmail.com>
2019-01-17 11:51:49 +0100

    made my own tcp admin client so I don't depend on socat anymore

---
+++ adminclient.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import socket
+import threading
+import os.path
+
+
+class AdminClient:
+	
+	def __init__(self):
+		self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+	
+	def start(self, address):
+		self.sock.connect(address)
+		threading.Thread(target=self.listen, daemon=True).start()
+		
+		while True:
+			command = input()
+			self.sock.sendall(bytes(command, "utf-8"))
+	
+	def listen(self):
+		while True:
+			data = self.sock.recv(2048)
+			print("")
+			print(data)
+
+def main():
+	address = os.path.join(os.path.dirname(__file__), "adminsocket.sock")
+	AdminClient().start(address)
+	
+
+
+if __name__ == "__main__":
+	main()
--- adminclient.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd "`dirname "$0"`"
-socat - UNIX-CONNECT:./adminsocket.sock
-
